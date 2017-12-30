e54127c2734c9072e1667a6da3884a3815316c1a
samjjc <sam111jc@gmail.com>
2017-12-30 16:45:40 -0500

    implement heap

--- cmd/pool/pool.go
+++ cmd/pool/pool.go
@@ -1,3 +1,27 @@
 package main
 
 type Pool []*Worker
+
+func (p Pool) Len() int {
+	return len(p)
+}
+
+func (p Pool) Less(i, j int) bool {
+	return p[i].pending < p[j].pending
+}
+
+func (p Pool) Swap(i, j int) {
+	p[i], p[j] = p[j], p[i]
+}
+
+func (p *Pool) Push(x interface{}) {
+	*p = append(*p, x.(*Worker))
+}
+
+func (p *Pool) Pop() interface{} {
+	old := *p
+	n := len(old)
+	x := old[n-1]
+	*p = old[0 : n-1]
+	return x
+}
