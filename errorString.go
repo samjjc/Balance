a63f869a14dab38faecfd181c643942536690c07
samjjc <sam111jc@gmail.com>
2017-12-31 13:43:37 -0500

    add request package

---
+++ errorString.go
@@ -0,0 +1,11 @@
+package request
+
+import "fmt"
+
+type ErrorString struct {
+	S string
+}
+
+func (e ErrorString) Error() string {
+	return fmt.Sprintf("work failed: %s", e.S)
+}
---
+++ request.go
@@ -0,0 +1,39 @@
+package request
+
+import (
+	"log"
+	"math/rand"
+	"time"
+)
+
+type Request struct {
+	Fn func() int
+	C  chan int
+	E  chan error
+}
+
+func Requester(work chan<- Request) {
+	c := make(chan int, 5)
+	e := make(chan error, 5)
+	for {
+		randDuration := time.Duration(rand.Intn(300)) * time.Millisecond
+		time.Sleep(randDuration + 100*time.Millisecond)
+		select {
+		case work <- Request{workFunc, c, e}:
+		case <-c:
+			// here would be where you'd process the results
+		case err := <-e:
+			log.Printf("%v\n", err)
+			work <- Request{workFunc, c, e}
+		}
+	}
+}
+
+func workFunc() int {
+	randDuration := time.Duration(rand.Intn(4000)) * time.Millisecond
+	time.Sleep(randDuration)
+	if randDuration <= time.Millisecond*800 {
+		panic("3 FAST 5 ME")
+	}
+	return 1
+}
